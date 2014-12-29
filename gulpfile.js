16cfd9a5bd4c8ce6cc3e3ff97dccbe1945113296
unknown <gusten86@gmail.com>
2014-12-29 19:57:08 +0100

    adding gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,32 @@
 var gulp = require('gulp');
 var to5 = require('gulp-6to5');
 var serve = require('gulp-serve');
+var watch = require('gulp-watch');
+
+var path = {
+	js: 'src/**/*.js',
+	html: 'src/**/*.html',
+	output: 'dist/'
+};
 
 gulp.task('build-js', function(){
-	return gulp.src('src/app.js')
+	return gulp.src(path.js)
 			.pipe(to5())
-			.pipe(gulp.dest('dist'));
+			.pipe(gulp.dest(path.output));
 });
 
 gulp.task('build-html', function(){
-	return gulp.src('src/app.html')			
-			.pipe(gulp.dest('dist'));
+	return gulp.src(path.html)			
+			.pipe(gulp.dest(path.output));
 });
 
 gulp.task('serve', ['build-js', 'build-html'], serve({ 
 	root: [__dirname],
 	port: 8000
 }));
+
+gulp.task('watch', ['serve'], function(){
+	gulp.watch([path.js, path.html], ['build-js', 'build-html']);
+});
 
 gulp.task('default', ['serve']);
